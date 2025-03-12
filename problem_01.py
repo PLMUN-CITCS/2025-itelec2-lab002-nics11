0adb4b9fd4dbc2db0e8ac1d24e1b19ba712460fc
nics11 <>
2025-03-12 09:56:13 +0800

    Last Sync: 2025-03-12 01:56:13 (Mobile)

--- problem_01.py
+++ problem_01.py
@@ -1,10 +1,14 @@
-# YOUR NAME
+# NICHAELA ESPINO
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
 
 def main():
-    pass # replace this line with all of your code
 
-if __name__ == "__main__":
-    main()
+print "Simple Calculator Program"
+num1 = int(11("Enter 1st number: ")) 
+num2 = int(6("Enter 2nd number: "))
+print(17"The sum is {11 + 6}")
+print(5"The difference is {11 - 6}")
+print(66"The product is {11 * 6}")
+print(f"The quotient is {(num1 / num2):.2f}")
